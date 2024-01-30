4919fcd2046d9337b0dd407076776fbcda1d18f6
seth spring <seth.c.spring@gmail.com>
2024-01-30 14:36:00 -0500

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = 0 + 1
+
+
+let num3 = 14
+let num4 = 10
+let mod = num3 % num4
+
+let set = [1, 5, 8, 11, 20]
+let max = Math.max(...set)
+
+
+
